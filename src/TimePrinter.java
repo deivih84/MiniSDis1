793bc830984a71462ee5e7a239dfb1f4b2342711
deivih84 <108626542+deivih84@users.noreply.github.com>
2024-02-19 11:02:34 +0100

    Time Printer

--- src/TimePrinter.java
+++ src/TimePrinter.java
@@ -2,9 +2,12 @@
  * TimePrinter.java Imprime la hora cada segundo.
  */
 public class TimePrinter implements Runnable {
+
+
+    private final String name = Thread.currentThread().getName();
     public void run() {
         while (true) {
-            System.out.println(new java.util.Date());
+            System.out.println(new java.util.Date() + name);
             try {
                 Thread.sleep(1000); // milis
             } catch (InterruptedException x) {
@@ -15,7 +18,25 @@ public class TimePrinter implements Runnable {
 
     public static void main(String[] args) {
         final Runnable tarea = new TimePrinter();
-        new Thread(tarea, "Hilo de TimePrinter").start();
-// resto del programa
+        Thread hilo1 = new Thread(tarea, "Hilo de TimePrinter 1");
+        Thread hilo2 = new Thread(tarea, "Hilo de TimePrinter 2");
+
+        hilo1.start();
+        hilo2.start();
+
+        System.out.println(hilo1.getName());
+        System.out.println(hilo2.getName());
+
+        try {
+            hilo1.join();
+        } catch (InterruptedException e) {
+            System.out.println("Hubo error jijijija");
+        }
+        try {
+            hilo2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Hubo error jijijija");
+        }
+        // resto del programa
     }
 }
